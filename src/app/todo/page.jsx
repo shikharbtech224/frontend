0f5deb95b963b8b4ce72602df764c1c9129b7cfd
shikharbtech224 <shikharrathore224@gmail.com>
2024-01-13 20:09:44 +0530

    todo

--- src/app/todo/page.jsx
+++ src/app/todo/page.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 
 const Todo = () => {
     const [todoList, setTodoList] = useState([]);
+    const nums = [2 , 5 , 7 , 9 , 3 ];
 
     const addTodo = (e) => {
         if(e.key ==='Enter'){
@@ -16,6 +17,21 @@ const Todo = () => {
 
         }
     }
+
+    const deleteTodo = (index) => {
+
+        const temp = todoList;
+        todoList.splice(index, 1);
+        setTodoList([...temp]);
+    }
+
+        const completeTodo = (index) => {
+            const temp = todoList;
+            temp[index].completed = !temp[index].completed;
+            setTodoList([...temp]);
+            console.log(temp);
+        }
+
   return (
     <div>
         <div className='container py-4'>
@@ -27,7 +43,21 @@ const Todo = () => {
                         <input onKeyDown={addTodo} type="text" className='form-control' />      
                     </div>
                 </div>
-            <div className='card-body border border-primary border-3'></div>
+            <div className='card-body border border-primary border-3'> 
+            {todoList.map( (todo, index) => {return <div className='d-flex justify-content-between mb-3 align-items-center'> 
+                <p>{todo.text}</p> 
+                {
+                    todo.completed ?
+                <span className='badge bg-success'>Complete</span>
+                :
+                <span className='badge bg-warning'>Incomplete</span>
+            }
+                <div>
+                    <button onClick={ () => { completeTodo(index) } } className='btn btn-primary me-3'>{todo.completed ? 'Undo' : 'Complete'}</button>
+                    <button onClick={ () => {deleteTodo(index) } } className='btn btn-danger'>Danger</button>
+                </div>
+                </div> } )} 
+            </div>
         </div>
     </div>
   )
